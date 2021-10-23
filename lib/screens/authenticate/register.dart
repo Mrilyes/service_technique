7081122f0de83bf47f0317843b218bafbe871fb4
Mr Ilyes <ilyesghlila1@gmail.com>
2021-10-24 00:38:56 +0100

    commit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:service_technique/services/auth.dart';
 import 'package:service_technique/shared/constants.dart';
+import 'package:service_technique/shared/loading.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -13,80 +14,89 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-  String error = '';
+  bool loading = false;
+
   @override
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Inscrivez-vous'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("s'identifier"),
-            onPressed: () => widget
-                .toggleView(), // widget refers to class Register extends StatefulWidget
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 30.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Inscrivez-vous'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text("s'identifier"),
+                  onPressed: () => widget
+                      .toggleView(), // widget refers to class Register extends StatefulWidget
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 30.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 30.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'password'),
+                      obscureText: true,
+                      validator: (val) =>
+                          val!.length < 6 ? 'Enter a password 6+' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 50.0),
+                    RaisedButton(
+                        color: Colors.pink[400],
+                        child: Text(
+                          'enregistrer',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                loading = false;
+                                error = 'Please supply a valid email';
+                              });
+                            }
+                          }
+                        }),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 30.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'password'),
-                obscureText: true,
-                validator: (val) =>
-                    val!.length < 6 ? 'Enter a password 6+' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 50.0),
-              RaisedButton(
-                  color: Colors.pink[400],
-                  child: Text(
-                    'enregistrer',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          error = 'Please supply a valid email';
-                        });
-                      }
-                    }
-                  }),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
