b32567838e286dfd9bce44667c709226e99b20c8
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-10-24 03:18:47 +0100

    refactor: adding comments

--- lib/screens/home/service_list.dart
+++ lib/screens/home/service_list.dart
@@ -15,49 +15,79 @@ class ServiceList extends StatefulWidget {
 }
 
 class _ServiceListState extends State<ServiceList> {
+  final reports = [
+    {
+      "id": '5515315383',
+      "nom": "Ilyes",
+      "prenom": "Ghorbel",
+      "email": "ilyesGh@gmail.com",
+      "pc": "Asus",
+      "tel": "52185821",
+      "desc": "description",
+    },
+    {
+      "id": '515834838',
+      "nom": "Test",
+      "prenom": "Testo",
+      "email": "test@test.com",
+      "pc": "HP",
+      "tel": "81856846",
+      "desc": "description",
+    },
+    {
+      "id": '321554238',
+      "nom": "Chabchoub",
+      "prenom": "Firas",
+      "email": "firaschabchoub@test.com",
+      "pc": "HP",
+      "tel": "52640818",
+      "desc": "description",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView(
-        children: [
-          ListTile(
-            leading: Icon(
-              Icons.account_box,
-              size: 40.0,
-            ),
-            title: Text(
-              "test@test.com",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text("le: 10/24/2021 - tel: 52185821"),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.delete_forever,
-                color: Colors.red,
-                size: 24.0,
-              ),
-              onPressed: () {},
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => Formulaire(
-                    id: '5515315383',
-                    nom: "Ilyes",
-                    prenom: "Ghorbel",
-                    email: "test@test.com",
-                    pc: "Asus",
-                    tel: "52185821",
-                    desc: "description",
-                  ),
-                ),
-              );
-            },
+        child: ListView.builder(
+      itemCount: reports.length,
+      itemBuilder: (_, i) {
+        return ListTile(
+          leading: Icon(
+            Icons.account_box,
+            size: 40.0,
           ),
-        ],
-      ),
-    );
+          title: Text(
+            reports[i]["email"].toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle:
+              Text("le: 10/24/2021 - tel: " + reports[i]["tel"].toString()),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.delete_forever,
+              color: Colors.red,
+              size: 24.0,
+            ),
+            onPressed: () {},
+          ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => Formulaire(
+                  id: reports[i]["id"],
+                  nom: reports[i]["nom"],
+                  prenom: reports[i]["prenom"],
+                  email: reports[i]["email"],
+                  pc: reports[i]["pc"],
+                  tel: reports[i]["tel"],
+                  desc: reports[i]["desc"],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    ));
   }
 }
