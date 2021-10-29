d6517c531a01dd4d6b6d2b2c61eaa35d52ae2b3a
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-10-29 23:39:14 +0100

    refactor: working on fetching data from firestore still

--- lib/screens/home/client_screen.dart
+++ lib/screens/home/client_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:service_technique/services/auth.dart';
@@ -16,134 +18,132 @@ class Client extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text('Espace Client'),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          actions: [
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('déconnecter'),
-              onPressed: () async {
-                await _auth.signOut();
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Espace Client'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('déconnecter'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: ' Nom',
+              ),
+              // validator: validateText,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer votre nom';
+                }
+                nom.text = value;
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: ' Prenon',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer votre prenon';
+                }
+                prenom.text = value;
+                return null;
+              },
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: ' Adresse E-mail',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer votre E-mail';
+                }
+                email.text = value;
+                return null;
+              },
+            ),
+            TextFormField(
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(
+                labelText: ' Numero De Telephone',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer votre Telephone';
+                }
+                tel.text = value;
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: ' Type De Ton PC',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer votre type de PC';
+                }
+                pc.text = value;
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'description de votre problème',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Veuillez entrer ton problème';
+                }
+                desc.text = value;
+                return null;
               },
             ),
           ],
         ),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: ' Nom',
-                ),
-                // validator: validateText,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer votre nom';
-                  }
-                  nom.text = value;
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: ' Prenon',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer votre prenon';
-                  }
-                  prenom.text = value;
-                  return null;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: ' Adresse E-mail',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer votre E-mail';
-                  }
-                  email.text = value;
-                  return null;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  labelText: ' Numero De Telephone',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer votre Telephone';
-                  }
-                  tel.text = value;
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: ' Type De Ton PC',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer votre type de PC';
-                  }
-                  pc.text = value;
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'description de votre problème',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer ton problème';
-                  }
-                  desc.text = value;
-                  return null;
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.save),
-          onPressed: () async {
-            // Validate returns true if the form is valid, or false otherwise
-            if (_formKey.currentState!.validate()) {
-              // Saving the report inthe database
-              await reports.add({
-                'Nom': nom.text,
-                'Prenom': prenom.text,
-                'Email': email.text,
-                'Tel': tel.text,
-                'PC': pc.text,
-                'Description': desc.text,
-              });
-              print("Form was submitted successfully.");
-              //Reset the Form
-              nom.text = "";
-              prenom.text = "";
-              email.text = "";
-              tel.text = "";
-              pc.text = "";
-              desc.text = "";
-              //Shows a snackbar with a message
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Ajoutée au succée')),
-              );
-            }
-          },
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.save),
+        onPressed: () async {
+          // Validate returns true if the form is valid, or false otherwise
+          if (_formKey.currentState!.validate()) {
+            // Saving the report inthe database
+            await reports.add({
+              'Nom': nom.text,
+              'Prenom': prenom.text,
+              'Email': email.text,
+              'Tel': tel.text,
+              'PC': pc.text,
+              'Description': desc.text,
+            });
+            print("Form was submitted successfully.");
+            //Reset the Form
+            nom.text = "";
+            prenom.text = "";
+            email.text = "";
+            tel.text = "";
+            pc.text = "";
+            desc.text = "";
+            //Shows a snackbar with a message
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Ajoutée au succée')),
+            );
+          }
+        },
       ),
     );
   }
--- lib/screens/home/service_list.dart
+++ lib/screens/home/service_list.dart
@@ -1,95 +1,40 @@
-// this widget is going to be responsible on outputting the difference declarations on the page
+// ignore_for_file: prefer_const_constructors, dead_code
 
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, void_checks, avoid_function_literals_in_foreach_calls
-
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:service_technique/models/rapport.dart';
-import 'package:service_technique/screens/home/formulaire/formulaire.dart';
+import 'package:flutter/material.dart';
 
-class ServiceList extends StatefulWidget {
-  const ServiceList({Key? key}) : super(key: key);
+class ServiceList extends StatelessWidget {
+  ServiceList({Key? key}) : super(key: key);
 
-  @override
-  _ServiceListState createState() => _ServiceListState();
-}
-
-List<Rapport>? rapports = [];
-
-class _ServiceListState extends State<ServiceList> {
-  void getData() async {
-    final reports =
-        await FirebaseFirestore.instance.collection('Reports').get();
-    final raps = reports.docs.map((doc) => doc.data()).toList();
-    raps.forEach((document) {
-      Rapport rapport = Rapport(
-        document["Nom"],
-        document["Prenom"],
-        document["Email"],
-        document["Tel"],
-        document["PC"],
-        document["Description"],
-      );
-      rapports!.add(rapport);
-    });
-    print(rapports!.length);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
+  var listOfWidgets = [];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: rapports!.length,
-        itemBuilder: (_, i) {
-          return ListTile(
-            leading: Icon(
-              Icons.account_box,
-              size: 40.0,
-            ),
-            title: Text(
-              rapports![i].email.toString(),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle:
-                Text("le: 10/24/2021 - tel: " + rapports![i].tel.toString()),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.delete_forever,
-                color: Colors.red,
-                size: 24.0,
-              ),
-              onPressed: () {
-                setState(() {
-                  rapports!.remove(rapports![i]);
-                });
-              },
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => Formulaire(
-                    id: "Rapport",
-                    nom: rapports![i].nom,
-                    prenom: rapports![i].prenom,
-                    email: rapports![i].email,
-                    pc: rapports![i].pc,
-                    tel: rapports![i].tel,
-                    desc: rapports![i].description,
-                  ),
-                ),
-              );
-            },
-          );
+    return ListView.builder(
+      itemCount: listOfWidgets.length,
+      itemBuilder: (BuildContext context, int index) => StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Reports").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Text("Pas De Rapports");
+          } else {
+            return ...listOfWidgets;
+          }
         },
       ),
     );
+  }
+  getReports(AsyncSnapshot<QuerySnapshot> snapshot) {
+    var report = snapshot.data!.docs
+        .map((doc) => ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text(doc["Email"]),
+              subtitle: Text(doc["Tel"]),
+              trailing: IconButton(
+                icon: Icon(Icons.delete_forever),
+                onPressed: () {},
+              ),
+            ))
+        .toList();
+        listOfWidgets.add(report);
   }
 }
