5083269d03fc6259ffecdcb39db09800e56fab1a
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-10-30 11:48:20 +0100

    refactor: finally fixed the data fetching, project is done

--- lib/screens/home/service_list.dart
+++ lib/screens/home/service_list.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_const_constructors, dead_code
 
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:service_technique/screens/home/formulaire/formulaire.dart';
 
 class ServiceList extends StatelessWidget {
   ServiceList({Key? key}) : super(key: key);
@@ -23,9 +22,32 @@ class ServiceList extends StatelessWidget {
           children: snapshot.data!.docs.map((doc) {
             return ListTile(
               leading: Icon(Icons.account_box),
-              title: Text(doc["Email"]),
-              subtitle: Text(doc["Tel"]),
-              onTap: () {},
+              title: Text(
+                doc["Email"],
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text("Tel: ${doc["Tel"]}"),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.delete_forever,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => Formulaire(
+                          id: doc.id,
+                          nom: doc["Nom"],
+                          prenom: doc["Prenom"],
+                          email: doc["Email"],
+                          tel: doc["Tel"],
+                          pc: doc["PC"],
+                          desc: doc["Description"],
+                        )));
+              },
             );
           }).toList(),
         );
